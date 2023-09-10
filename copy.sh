3f4770159dd6b9c035bc1016ea5c7036f24968b4
ohtuhantolongaku <144606613+ohtuhantolongaku@users.noreply.github.com>
2023-09-11 05:52:33 +0700

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,3 +1,3 @@
 # curl https://rclone.org/install.sh | bash
 echo -e "$RCLONE_CONFIG" > rclone.conf
-rclone copy -P "$SOURCE_RC" "$DESTINATION_RC" --config=rclone.conf
+rclone copy --use-mmap --buffer-size 0 --transfers 1 --checkers 1 -P "$SOURCE_RC" "$DESTINATION_RC" --config=rclone.conf
