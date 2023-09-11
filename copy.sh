a26e4f7f7d0812c789130bdbc307845cf9ee3135
ohtuhantolongaku <144606613+ohtuhantolongaku@users.noreply.github.com>
2023-09-12 05:34:23 +0700

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,3 +1,7 @@
+pip install flask
+pip install flask_restful
 # curl https://rclone.org/install.sh | bash
 echo -e "$RCLONE_CONFIG" > rclone.conf
+
+python3 web.py &
 rclone copy -P "$SOURCE_RC" "$DESTINATION_RC" --config=rclone.conf
